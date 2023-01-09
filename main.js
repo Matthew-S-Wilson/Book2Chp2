44ea6abb7e9e1f28c9d02e54331690a4bab434b1
Matthew-S-Wilson <matthew255@gmail.com>
2023-01-09 10:45:34 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (quartered) => {
+    quarter = quartered / 4
+    return quarter
+    }
+    
+    // Invoke the function and store the return value
+    
+    const returnedNumber = quarterValue(16)
+    // Log the return value
+    console.log(returnedNumber)
